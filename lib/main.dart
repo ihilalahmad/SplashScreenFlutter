54ab8afd52a01f184b434834d146c27cd0b3f4e3
softcoding <itssoftcpding@gmail.com>
2021-05-05 16:50:00 +0500

    initial commit.

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,7 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       backgroundColor: Colors.blue,
       body: Column(
